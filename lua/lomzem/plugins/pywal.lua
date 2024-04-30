0fcffe962ad40544f807ad88233c37a617453cd7
Lawjay Lee <lawjaylee1@icloud.com>
2024-04-29 21:14:57 -0700

    diff pywal plugin

--- lua/lomzem/plugins/pywal.lua
+++ lua/lomzem/plugins/pywal.lua
@@ -1,8 +1,10 @@
 return {
-	"oncomouse/lushwal.nvim",
-	cmd = { "LushwalCompile" },
-	dependencies = {
-		{ "rktjmp/lush.nvim" },
-		{ "rktjmp/shipwright.nvim" },
-	},
+	"mbrea-c/wal-colors.nvim",
+	config = function()
+		vim.cmd([[colorscheme mbc]]) -- activate the colorscheme
+		vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+	end,
+	priority = 1000, -- recommended to ensure the colorscheme
+	-- is loaded before other plugins
 }
--- lua/lomzem/set-colorscheme.lua
+++ lua/lomzem/set-colorscheme.lua
@@ -3,23 +3,5 @@
 -- vim.cmd("colorscheme catppuccin")
 -- vim.cmd("colorscheme catppuccin-mocha")
 
-vim.g.lushwal_configuration = {
-	addons = {
-		lualine = true,
-	},
-}
-
-vim.g.lushwal_configuration = {
-	color_overrides = function(colors)
-		local overrides = {
-			red = colors.color5,
-			orange = colors.color1,
-		}
-		return vim.tbl_extend("force", colors, overrides)
-	end,
-	-- ...
-}
-vim.cmd("LushwalCompile")
-vim.cmd("colorscheme lushwal")
-vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+-- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
