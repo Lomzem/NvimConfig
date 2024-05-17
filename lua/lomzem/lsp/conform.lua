8d87c1e9a58359136c4eb4febf779771ba50feea
Lawjay Lee <lawjaylee1@icloud.com>
2024-05-17 14:34:30 -0700

    fix conform?

--- lua/lomzem/lsp/conform.lua
+++ lua/lomzem/lsp/conform.lua
@@ -1,10 +1,10 @@
 require("conform").setup({
-	format_on_save = {
+	format_after_save = {
 		async = true,
 		lsp_fallback = true,
 	},
 	-- I miss autoformat :(
-	-- format_on_save = function() end,
+	-- format_after_save = function() end,
 	formatters_by_ft = {
 		go = { "gofmt" },
 		lua = { "stylua" },
