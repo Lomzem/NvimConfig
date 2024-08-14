fdc3e7987fe455f12729d0c0daa0a535492e94f5
Lawjay Lee <lawjaylee1@icloud.com>
2024-08-13 20:32:29 -0700

    no more lazy notify

--- lua/lomzem/lazy.lua
+++ lua/lomzem/lazy.lua
@@ -11,4 +11,8 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("lomzem.plugins")
+require("lazy").setup({ import = "lomzem.plugins" }, {
+	change_detection = {
+		notify = false,
+	},
+})
