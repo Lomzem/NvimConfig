b471c89feeb623c38582f581ebf2b8a0e6dd9f51
Lawjay Lee <lawjaylee1@icloud.com>
2024-09-02 18:44:50 -0700

    telescope lsp

--- lua/lomzem/lsp/lsp_mapping.lua
+++ lua/lomzem/lsp/lsp_mapping.lua
@@ -6,15 +6,17 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		local opts = { buffer = ev.buf }
 
 		vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-		vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-		vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, opts)
+		-- vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+		-- vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, opts)
+		vim.keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts)
+		vim.keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts)
 		vim.keymap.set("n", "gr", "<cmd>Telescope lsp_references<CR>", opts)
 		vim.keymap.set("n", "<leader>mb", "<cmd>Telescope lsp_document_symbols<CR>", opts)
 
 		vim.keymap.set("n", "<leader>di", "<cmd>Telescope diagnostics<CR>", opts)
 		vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, opts)
-		vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
-		vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+		-- vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+		-- vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
 
 		vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
 		vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
--- lua/lomzem/set-colorscheme.lua
+++ lua/lomzem/set-colorscheme.lua
@@ -1,10 +1,11 @@
 -- vim.cmd("colorscheme rose-pine-main")
-vim.cmd("colorscheme rose-pine-moon")
+-- vim.cmd("colorscheme rose-pine-moon")
 -- vim.cmd("colorscheme gruvbox")
 -- vim.cmd("colorscheme catppuccin")
 -- vim.cmd("colorscheme catppuccin-frappe")
 -- vim.cmd("colorscheme catppuccin-mocha")
 -- vim.cmd("colorscheme tokyonight-moon")
+vim.cmd("colorscheme tokyonight-night")
 
 -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
