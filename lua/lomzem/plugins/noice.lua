7e8d0691019b7dc0fd5338dc238fbebb4b2d40d5
Lawjay Lee <lawjaylee1@icloud.com>
2024-09-15 16:19:59 -0700

    noice

--- lua/lomzem/plugins/noice.lua
+++ lua/lomzem/plugins/noice.lua
@@ -7,6 +7,12 @@ return {
 	},
 	dependencies = {
 		"MunifTanjim/nui.nvim",
-		{ "rcarriga/nvim-notify", opts = { background_colour = "#000000" } },
+		{
+			"rcarriga/nvim-notify",
+			opts = {
+				background_colour = "#000000",
+				render = "compact",
+			},
+		},
 	},
 }
