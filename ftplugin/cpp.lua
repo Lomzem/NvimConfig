e1d0ba68848db8617232a5b715d54031a0963a71
Lawjay Lee <lawjaylee1@icloud.com>
2024-10-06 16:15:44 -0700

    makefile cpp

--- ftplugin/cpp.lua
+++ ftplugin/cpp.lua
@@ -1,2 +1,20 @@
-vim.keymap.set("n", "<leader>ru", ":w<cr>:term make<cr>")
+-- Check if Makefile exists
+local makefile_path = vim.fn.fnamemodify(vim.fn.expand("%:p"), ":h") .. "/Makefile"
+
+local function file_exists(name)
+	local f = io.open(name, "r")
+	if f ~= nil then
+		io.close(f)
+		return true
+	else
+		return false
+	end
+end
+
+if file_exists(makefile_path) then
+	vim.keymap.set("n", "<leader>ru", ":w<cr>:term make<cr>")
+else
+	vim.keymap.set("n", "<leader>ru", ":w<cr>:term g++ % && ./a.out && rm ./a.out<cr>")
+end
+
 require("lomzem/lsp/snippets/cpp")
