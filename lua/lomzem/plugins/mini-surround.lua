bb1f491348203ce787c734ac833e78ab64f7aa5b
Lawjay Lee <lawjaylee1@icloud.com>
2024-10-28 19:09:42 -0700

    surround S mapping

--- lua/lomzem/plugins/mini-surround.lua
+++ lua/lomzem/plugins/mini-surround.lua
@@ -2,7 +2,7 @@ return {
 	"echasnovski/mini.surround",
 	opts = {
 		mappings = {
-			add = "ys", -- Add surrounding in Normal and Visual modes
+			add = "S", -- Add surrounding in Normal and Visual modes
 			delete = "ds", -- Delete surrounding
 			find = "", -- Find surrounding (to the right) default: sf
 			find_left = "", -- Find surrounding (to the left) default: sF
