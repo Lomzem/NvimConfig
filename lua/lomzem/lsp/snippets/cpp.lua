411066b7448a3df0c3c2f6d3802e47f3a9aac4f8
Lawjay Lee <lawjaylee1@icloud.com>
2024-11-07 17:06:12 -0800

    Just push :P

--- lua/lomzem/lsp/snippets/cpp.lua
+++ lua/lomzem/lsp/snippets/cpp.lua
@@ -41,7 +41,7 @@ ls.add_snippets("cpp", {
 		name = "Traditional For Loop",
 		dscr = "for (int i(0); i < $1; i++) {}",
 	}, {
-		t("for (int i(0); i < "),
+		t("for (int i = 0; i < "),
 		i(1),
 		t({ "; i++) {", "\t" }),
 		i(2),
