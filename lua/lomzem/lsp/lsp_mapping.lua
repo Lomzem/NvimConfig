f4bcaddd0eb056706bb4985f0618ad94e1ca34f4
Lawjay Lee <lawjaylee1@icloud.com>
2024-11-11 12:40:34 -0800

    Just push :P

--- lua/lomzem/lsp/lsp_mapping.lua
+++ lua/lomzem/lsp/lsp_mapping.lua
@@ -13,8 +13,8 @@ vim.api.nvim_create_autocmd("LspAttach", {
 
 		vim.keymap.set("n", "<leader>di", "<cmd>Telescope diagnostics<CR>", opts)
 		vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, opts)
-		-- vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
-		-- vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+		vim.keymap.set("n", "gnd", vim.diagnostic.goto_next, opts)
+		vim.keymap.set("n", "gld", vim.diagnostic.goto_prev, opts)
 
 		vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
 		vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
