7e1d16c9f4bdcd7269f37caa45cf08c989faea11
Lawjay Lee <lawjaylee1@icloud.com>
2024-11-20 13:16:08 -0800

    Just push :P

--- lua/lomzem/plugins/obsidian.lua
+++ lua/lomzem/plugins/obsidian.lua
@@ -5,19 +5,19 @@ return {
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 	},
-	opts = {
-		workspaces = {
-			{
-				name = "Chico Obsidian",
-				path = "~/coding/chico-obsidian",
-			},
-		},
-		completion = {
-			nvim_cmp = true,
-			min_chars = 2,
-		},
-	},
 	config = function()
 		vim.opt.conceallevel = 2 -- for obsidian
+		require("obsidian").setup({
+			workspaces = {
+				{
+					name = "Chico Obsidian",
+					path = "~/coding/chico-obsidian",
+				},
+			},
+			completion = {
+				nvim_cmp = true,
+				min_chars = 2,
+			},
+		})
 	end,
 }
