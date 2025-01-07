3f8f1d274d52b0ac5eca8beace6a0aaae18208d0
Lawjay Lee <lawjaylee1@icloud.com>
2025-01-07 12:10:06 -0800

    Just push :P

--- lua/lomzem/vimopts.lua
+++ lua/lomzem/vimopts.lua
@@ -19,7 +19,8 @@ vim.opt.smartcase = true
 
 -- line wrapping
 vim.opt.wrap = false
-vim.opt.scrolloff = 8
+-- vim.opt.scrolloff = 8
+vim.opt.scrolloff = 999
 
 -- appearance
 vim.opt.termguicolors = true
