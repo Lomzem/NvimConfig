cbb5f06f9607be43e1751444664ba9363a115217
Lawjay Lee <lawjaylee1@icloud.com>
2025-02-06 11:28:01 -0800

    Just push :P

--- lua/lomzem/lsp/lsp_mapping.lua
+++ lua/lomzem/lsp/lsp_mapping.lua
@@ -4,22 +4,14 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		local opts = { buffer = ev.buf }
 
 		vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-		vim.keymap.set("n", "gd", "<cmd>FzfLua lsp_definitions<CR>", opts)
-		vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-		vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, opts)
-		vim.keymap.set("n", "gr", "<cmd>FzfLua lsp_references<CR>", opts)
-		vim.keymap.set("n", "<leader>mb", "<cmd>FzfLua lsp_document_symbols<CR>", opts)
 
-		vim.keymap.set("n", "<leader>di", "<cmd>FzfLua lsp_workspace_diagnostics<CR>", opts)
 		vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, opts)
-		vim.keymap.set("n", "gnd", vim.diagnostic.goto_next, opts)
-		vim.keymap.set("n", "gld", vim.diagnostic.goto_prev, opts)
+		vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+		vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
 
 		vim.keymap.set("n", "U", vim.lsp.buf.code_action, opts)
 		vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
-
 		vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-
 		vim.keymap.set("n", "<leader>f", "<cmd>Format<CR>", opts)
 	end,
 })
--- lua/lomzem/plugins/fzf-lua.lua
+++ lua/lomzem/plugins/fzf-lua.lua
@@ -1,14 +1,15 @@
-return {
-	"ibhagwan/fzf-lua",
-	lazy = true,
-	opts = {
-		fzf_opts = {
-			["--select-1"] = true,
-		},
-	},
-	cmd = "FzfLua",
-	keys = {
-		{ "<C-p>", "<cmd>FzfLua files<cr>" },
-		{ "<leader>ps", "<cmd>FzfLua live_grep<cr>" },
-	},
-}
+return {}
+-- return {
+-- 	"ibhagwan/fzf-lua",
+-- 	lazy = true,
+-- 	opts = {
+-- 		fzf_opts = {
+-- 			["--select-1"] = true,
+-- 		},
+-- 	},
+-- 	cmd = "FzfLua",
+-- 	keys = {
+-- 		{ "<C-p>", "<cmd>FzfLua files<cr>" },
+-- 		{ "<leader>ps", "<cmd>FzfLua live_grep<cr>" },
+-- 	},
+-- }
