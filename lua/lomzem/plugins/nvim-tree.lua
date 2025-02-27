762bef760ac1f8dc814831904174b9fc8118769d
Lawjay Lee <lawjaylee1@icloud.com>
2025-02-27 14:14:38 -0800

    Just push :P

---
+++ lua/lomzem/plugins/nvim-tree.lua
@@ -0,0 +1,50 @@
+return {
+	"nvim-tree/nvim-tree.lua",
+	lazy = false,
+	config = function()
+		local api = require("nvim-tree.api")
+
+		local function edit_or_open()
+			local node = api.tree.get_node_under_cursor()
+
+			if node.nodes ~= nil then
+				-- expand or collapse folder
+				api.node.open.edit()
+			else
+				-- open file
+				api.node.open.edit()
+				-- Close the tree if file was opened
+				api.tree.close()
+			end
+		end
+
+		local function vsplit_preview()
+			local node = api.tree.get_node_under_cursor()
+
+			if node.nodes ~= nil then
+				-- expand or collapse folder
+				api.node.open.edit()
+			else
+				-- open file as vsplit
+				api.node.open.vertical()
+			end
+
+			-- Finally refocus on tree if it was lost
+			api.tree.focus()
+		end
+
+		require("nvim-tree").setup({
+			on_attach = function(bufnr)
+				vim.keymap.set("n", "l", edit_or_open)
+				vim.keymap.set("n", "<cr>", edit_or_open)
+				vim.keymap.set("n", "L", vsplit_preview)
+				vim.keymap.set("n", "h", api.tree.close)
+				vim.keymap.set("n", "q", api.tree.close)
+				vim.keymap.set("n", "H", api.tree.collapse_all)
+			end,
+		})
+	end,
+	keys = {
+		{ "<c-h>", "<cmd>NvimTreeToggle<cr>" },
+	},
+}
