a18343e4d4d8aa8935fc9e03fa5d296929a4b3b3
Lawjay Lee <lawjaylee1@icloud.com>
2025-03-28 19:11:32 -0700

    Just push :P

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -38,7 +38,7 @@ return {
 				["<c-l>"] = { "select_and_accept" },
 				["<c-k>"] = { "select_prev", "fallback" },
 				["<c-j>"] = { "select_next", "fallback" },
-				["<CR>"] = { "accept_and_enter", "fallback" },
+				["<cr>"] = { "accept_and_enter", "fallback" },
 			},
 			completion = { menu = { auto_show = true } },
 		},
