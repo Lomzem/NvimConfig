cec51cbd19dc6979fb4cf97c236d068ba0061782
Lawjay Lee <lawjaylee1@icloud.com>
2025-04-30 11:34:33 -0700

    Just push :P

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -8,6 +8,7 @@ return {
 				["vim.lsp.util.stylize_markdown"] = true,
 				["cmp.entry.get_documentation"] = false,
 			},
+			signature = { enabled = false },
 		},
 		presets = {
 			bottom_search = true, -- use a classic bottom cmdline for search
