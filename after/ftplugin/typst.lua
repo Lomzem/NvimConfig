553e3f44ed559e42d43af7a10f979906044db1ec
Lawjay Lee <lawjaylee1@icloud.com>
2025-05-07 20:31:15 -0700

    Just push 😝

--- after/ftplugin/typst.lua
+++ after/ftplugin/typst.lua
@@ -2,10 +2,20 @@ vim.cmd("set spell")
 vim.cmd("set wrap")
 vim.cmd("set linebreak")
 
+vim.o.textwidth = 80
+
+-- Automatically wrap lines when typing
+vim.opt.formatoptions:append("t")
+-- Auto-wrap comments and use comment leaders (like //)
+-- vim.opt.formatoptions:append('c')
+-- Auto-insert current comment leader when hitting Enter in insert mode
+-- vim.opt.formatoptions:append('r')
+-- Auto-insert current comment leader when hitting o/O in normal mode
+-- vim.opt.formatoptions:append('o')
+
 vim.api.nvim_create_autocmd("BufWritePost", {
 	pattern = "*.typ",
 	callback = function()
 		vim.cmd("silent !typst compile %")
 	end,
 })
-
--- lua/plugins/marks-nvim.lua
+++ lua/plugins/marks-nvim.lua
@@ -1,5 +1,11 @@
 return {
 	"chentoast/marks.nvim",
 	event = "VeryLazy",
-	opts = {},
+	opts = {
+		mappings = {
+			set_next = "m,",
+			delete_buf = "dm<leader>",
+			next = "<a-tab>",
+		},
+	},
 }
