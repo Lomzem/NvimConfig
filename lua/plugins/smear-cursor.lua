00948d0f8f3c35f9aba333b99ed2f43f642bfb79
Lawjay Lee <lawjaylee1@icloud.com>
2025-05-09 13:22:37 -0700

    Just push 😝

--- lua/plugins/smear-cursor.lua
+++ lua/plugins/smear-cursor.lua
@@ -1,9 +1,10 @@
 return {
 	"sphamba/smear-cursor.nvim",
+	enabled = false,
 	opts = {
 		stiffness = 0.6, -- 0.6      [0, 1]
 		trailing_stiffness = 0.3, -- 0.3      [0, 1]
 		distance_stop_animating = 0.5, -- 0.1      > 0
-        legacy_computing_symbols_support = true, -- better for transparent backgrounds
+		legacy_computing_symbols_support = true, -- better for transparent backgrounds
 	},
 }
