817929ac0d81b74b8ebda468696785f0ec95f662
Lawjay Lee <lawjaylee1@icloud.com>
2025-05-09 14:11:36 -0700

    Just push 😝

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -3,10 +3,9 @@ return {
 	lazy = true,
 	version = false, -- Never set this value to "*"! Never!
 	keys = {
-		"v",
-		"<leader>aa",
-		"AvanteAsk",
+		{ "<leader>aa", "<cmd>AvanteAsk<cr>", mode = { "n", "v" } },
 	},
+	cmd = "AvanteAsk",
 	opts = {
 		selector = {
 			--- @alias avante.SelectorProvider "native" | "fzf_lua" | "mini_pick" | "snacks" | "telescope" | fun(selector: avante.ui.Selector): nil
