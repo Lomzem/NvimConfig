bd65e5c50bd990f69ac344858bc6bfdab747bf5b
Lawjay Lee <lawjaylee1@icloud.com>
2025-05-13 11:51:02 -0700

    Just push 😝

--- lua/plugins/colorschemes/poimandres.lua
+++ lua/plugins/colorschemes/poimandres.lua
@@ -2,10 +2,16 @@ return {
 	"olivercederborg/poimandres.nvim",
 	lazy = false,
 	priority = 1000,
-	config = function()
-		require("poimandres").setup({
+	opts = {
+		{
 			-- disable_background = true,
 			-- disable_float_background = true,
-		})
+		},
+	},
+	config = function()
+		vim.api.nvim_set_hl(0, "@markup.heading.1.markdown", { link = "markdownH1" })
+		vim.api.nvim_set_hl(0, "@markup.heading.2.markdown", { link = "markdownH2" })
+		vim.api.nvim_set_hl(0, "@markup.heading.3.markdown", { link = "markdownH3" })
+		vim.api.nvim_set_hl(0, "@markup.heading.4.markdown", { link = "markdownH4" })
 	end,
 }
