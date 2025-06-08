290edbf5b3e8dad8b32cefdfd5a520114c74f54b
Lawjay Lee <lawjaylee1@icloud.com>
2025-06-07 18:40:57 -0700

    Just push 😝

--- lua/lsp/snippets/cpp.lua
+++ lua/lsp/snippets/cpp.lua
@@ -1,7 +1,9 @@
 local ls = require("luasnip")
 local s = ls.snippet
 local t = ls.text_node
--- local i = ls.insert_node
+local i = ls.insert_node
+local fmt = require("luasnip.extras.fmt").fmt
+local fmta = require("luasnip.extras.fmt").fmta
 
 -- local extras = require("luasnip.extras")
 -- local rep = extras.rep
@@ -23,4 +25,26 @@ ls.add_snippets("cpp", {
 	}, {
 		t("using namespace std;"),
 	}),
+
+	s("/", fmt([[/* {} */{}]], { i(1), i(0) })),
+	s("i", fmt([[#include <{}>{}]], { i(1), i(0) })),
+	s(
+		"for",
+		fmta(
+			[[
+for (auto i = 0; i << <>.size(); i++) {
+    <>
+}]],
+			{ i(1), i(0) }
+		)
+	),
+	s(
+		"forca",
+		fmta(
+			[[for (const auto &<> : <>) {
+    <>
+}]],
+			{ i(1), i(2), i(0) }
+		)
+	),
 })
--- lua/plugins/rainbow-delimiters.lua
+++ lua/plugins/rainbow-delimiters.lua
@@ -1,16 +1,16 @@
 return {
 	"HiPhish/rainbow-delimiters.nvim",
 	main = "rainbow-delimiters.setup",
-	cond = function()
-		local excluded_cs = { "poimandres" }
-		local cur_cs = vim.g.colors_name
-
-		for _, val in ipairs(excluded_cs) do
-			if val == cur_cs then
-				return false
-			end
-		end
-		return false
-	end,
+	-- cond = function()
+	-- 	local excluded_cs = { "poimandres" }
+	-- 	local cur_cs = vim.g.colors_name
+	--
+	-- 	for _, val in ipairs(excluded_cs) do
+	-- 		if val == cur_cs then
+	-- 			return false
+	-- 		end
+	-- 	end
+	-- 	return false
+	-- end,
 	opts = {},
 }
