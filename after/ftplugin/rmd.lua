f06ef1ed134656289e35b685d07a45c0e5593c8e
Lawjay Lee <lawjaylee1@icloud.com>
2025-09-02 15:05:33 -0700

    Just push 😝

--- after/ftplugin/rmd.lua
+++ after/ftplugin/rmd.lua
@@ -7,14 +7,6 @@ vim.api.nvim_create_autocmd("BufWritePost", {
 			"-e",
 			"rmarkdown::render('" .. file_path .. "')",
 		}
-		vim.fn.jobstart(cmd, {
-			-- on_exit = function(job_id, exit_code, event)
-			-- if exit_code == 0 then
-			-- 	vim.notify("Rmd compilation finished successfully.", vim.log.levels.INFO, {})
-			-- else
-			-- 	vim.notify("Rmd compilation failed with exit code: " .. exit_code, vim.log.levels.ERROR, {})
-			-- end
-			-- end,
-		})
+		vim.fn.jobstart(cmd)
 	end,
 })
