5407790de10568a7ff279d5dd486b5b57684eca4
Lawjay Lee <lawjaylee1@icloud.com>
2025-09-02 19:54:55 -0700

    Just push 😝

--- after/ftplugin/rmd.lua
+++ after/ftplugin/rmd.lua
@@ -2,15 +2,10 @@ vim.api.nvim_create_autocmd("BufWritePost", {
 	pattern = "*.Rmd",
 	callback = function()
 		local file_path = vim.fn.expand("%:p")
-		local cmd = {
-			"Rscript",
-			"-e",
-			"rmarkdown::render('" .. file_path .. "')",
-		}
-		vim.fn.jobstart(cmd, {
-			on_stderr = function() end,
-			on_stdout = function() end,
-			on_exit = function() end,
+		vim.system({ "Rscript", "-e", "rmarkdown::render('" .. file_path .. "')" }, {
+			stdout = false,
+			stderr = false,
+			detach = true,
 		})
 	end,
 })
