13ec1357d31964778c73e14997032b3fad16b9cb
Lawjay Lee <lawjaylee1@icloud.com>
2025-10-07 21:40:05 -0700

    Just push 😝

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,7 +1,7 @@
 vim.g.mapleader = " "
 
 -- General
-vim.keymap.set("v", "+", "<Esc>ggVG")
+vim.keymap.set("v", "+", "<esc>ggVG")
 
 vim.keymap.set("n", "y=", function()
 	local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
@@ -11,30 +11,30 @@ vim.keymap.set("n", "y=", function()
 end) -- Copy all
 vim.keymap.set("n", "<leader>q", "<c-z>") -- Suspend
 vim.keymap.set("n", "<a-q>", "<cmd>q<CR>") -- Quit
-vim.keymap.set("i", "<C-c>", "<Esc>") -- Remap Ctrl+C to Esc
+vim.keymap.set("i", "<c-c>", "<esc>") -- Remap Ctrl+C to Esc
 
 -- Quickfix
 vim.keymap.set("n", "<A-n>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<A-p>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<A-d>", "<cmd>cclose<CR>")
 
-vim.keymap.set("n", "<leader>z=", "1z=") --spellcheck
+vim.keymap.set("n", "z=", "1z=") --spellcheck
 
-vim.keymap.set("n", "<C-s>", "<cmd>w<CR>") -- Save
-vim.keymap.set("i", "<C-s>", "<Esc><cmd>w<CR>") -- Save
--- vim.keymap.set("n", ";w", "<Esc><cmd>w<CR>") -- Save
-vim.keymap.set("n", "<leader>w", "<Esc><cmd>w<CR>") -- Save
-vim.keymap.set("n", "<leader>x", "<Esc><cmd>x<CR>") -- Save
+vim.keymap.set("n", "<c-s>", "<cmd>silent w<cr>") -- Save
+vim.keymap.set("i", "<c-s>", "<esc><cmd>silent w<cr>") -- Save
+-- vim.keymap.set("n", ";w", "<esc><cmd>silent w<cr>", {silent=true}) -- Save
+vim.keymap.set("n", "<leader>w", "<esc><cmd>silent w<cr>") -- Save
+vim.keymap.set("n", "<leader>x", "<esc><cmd>silent x<cr>") -- Save
 vim.keymap.set("n", "<cr>", "zz")
 
 -- Centering
-vim.keymap.set("n", "<C-u>", "<C-u>zz") -- Centers page up
-vim.keymap.set("n", "<C-d>", "<C-d>zz") -- Centers page d-own
+vim.keymap.set("n", "<c-u>", "<c-u>zz") -- Centers page up
+vim.keymap.set("n", "<c-d>", "<c-d>zz") -- Centers page d-own
 vim.keymap.set("n", "n", "nzzzv") -- Centers search forward
 vim.keymap.set("n", "N", "Nzzzv") -- Centers search backwards
--- vim.keymap.set("n", "<C-i>", "<C-i>zz")
--- vim.keymap.set("n", "<C-o>", "<C-o>zz")
--- vim.keymap.set("n", "<C-t>", "<C-t>zz") -- Centers tag stack
+-- vim.keymap.set("n", "<c-i>", "<c-i>zz")
+-- vim.keymap.set("n", "<c-o>", "<c-o>zz")
+-- vim.keymap.set("n", "<c-t>", "<c-t>zz") -- Centers tag stack
 
 -- vim.keymap.set("n", ";", ":") -- Who likes pressing shift?
 
@@ -51,8 +51,8 @@ vim.keymap.set("n", "D", "dd")
 -- vim.keymap.set("n", "k", "kzz")
 vim.keymap.set("n", "G", "G")
 vim.keymap.set("n", "J", "<cmd>let p=getpos('.')<bar>join<bar>call setpos('.', p)<cr>") -- Keeps cursor in place
-vim.keymap.set("n", "<leader>s", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>") -- Replace current word
-vim.keymap.set("i", "<C-x>", "<C-o>x")
+vim.keymap.set("n", "<leader>s", ":%s/\\<<c-r><c-w>\\>/<c-r><c-w>/gI<Left><Left><Left>") -- Replace current word
+vim.keymap.set("i", "<c-x>", "<c-o>x")
 
 -- jk jumplist
 vim.keymap.set({ "n", "x" }, "j", function()
