ebe9da5efd2c7547f69023904a3411ea524f41af
Lawjay Lee <lawjaylee1@icloud.com>
2025-10-20 19:05:53 -0700

    Just push 😝

--- lua/highlights.lua
+++ lua/highlights.lua
@@ -1,1 +1,7 @@
 vim.cmd.highlight("LineNr", "guibg=none")
+-- vim.api.nvim_set_hl(0, "Normal", {
+-- 	bg = "none",
+-- })
+-- vim.api.nvim_set_hl(0, "NormalFloat", {
+-- 	bg = "none",
+-- })
---
+++ lua/plugins/colorschemes/synthweave.lua
@@ -0,0 +1,9 @@
+---@type LazySpec
+return {
+	"samharju/synthweave.nvim",
+	lazy = false,
+	priority = 1000,
+	---@module "synthweave"
+	---@type Config
+	opts = {},
+}
