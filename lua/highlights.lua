c3498af5f9ce2662ee92be5bb768fd33c5c4ab20
Lawjay Lee <lawjaylee1@icloud.com>
2025-10-22 12:01:11 -0700

    Just push 😝

--- lua/highlights.lua
+++ lua/highlights.lua
@@ -1,7 +1,18 @@
 vim.cmd.highlight("LineNr", "guibg=none")
--- vim.api.nvim_set_hl(0, "Normal", {
--- 	bg = "none",
--- })
--- vim.api.nvim_set_hl(0, "NormalFloat", {
--- 	bg = "none",
--- })
+
+local groups = {
+	"FloatBorder",
+	"NonText",
+	"Normal",
+	"NormalNC",
+	"SignColumn",
+	"StatusLine",
+	"StatusLineNC",
+	"NormalFloat",
+}
+
+for _, group in ipairs(groups) do
+	vim.api.nvim_set_hl(0, group, {
+		bg = "none",
+	})
+end
