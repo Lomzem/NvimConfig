1b916ceaf22174d6c34ad301a8f35a23e74659f1
Lawjay Lee <lawjaylee1@icloud.com>
2025-10-27 15:41:47 -0700

    Just push 😝

--- after/ftplugin/tex.lua
+++ after/ftplugin/tex.lua
@@ -1,7 +1,12 @@
 require("lsp/snippets/tex")
-vim.cmd("set spell")
-vim.cmd("set wrap")
-vim.cmd("set linebreak")
+vim.opt_local.spell = true
+vim.opt_local.wrap = true
+vim.opt_local.linebreak = true
+
+vim.keymap.set("n", "j", "gj", { silent = true, buffer = 0 })
+vim.keymap.set("n", "k", "gk", { silent = true, buffer = 0 })
+vim.keymap.set("n", "H", "g0", { silent = true, buffer = 0 })
+vim.keymap.set("n", "L", "g$", { silent = true, buffer = 0 })
 
 vim.api.nvim_create_autocmd("BufWritePost", {
 	pattern = "*.tex",
