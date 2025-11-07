9bfe524145c8ef65a129288b3aa9359ce20ef1a6
Lawjay Lee <lawjaylee1@icloud.com>
2025-11-06 23:31:25 -0800

    Just push 😝

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,22 +1,17 @@
 ---@type LazySpec[]
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		lazy = false,
-		build = ":TSUpdate",
-		main = "nvim-treesitter.configs",
-		opts = {
-			sync_install = false,
-			auto_install = true,
-			highlight = {
-				enable = true,
-				additional_vim_regex_highlighting = false,
-			},
-		},
-	},
-	{
-		"bezhermoso/tree-sitter-ghostty",
-		build = "make nvim_install",
-		ft = { "ghostty" },
-	},
+    {
+        "nvim-treesitter/nvim-treesitter",
+        lazy = false,
+        build = ":TSUpdate",
+        main = "nvim-treesitter.configs",
+        opts = {
+            sync_install = false,
+            auto_install = true,
+            highlight = {
+                enable = true,
+                additional_vim_regex_highlighting = false,
+            },
+        },
+    },
 }
