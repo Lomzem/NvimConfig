2ba7ea217441d788a868687781b8d8523ad76b10
Lawjay Lee <lawjaylee1@icloud.com>
2025-11-13 14:33:29 -0800

    Just push 😝

--- lua/plugins/colorschemes/neopywal.lua
+++ lua/plugins/colorschemes/neopywal.lua
@@ -1,9 +1,16 @@
+---@type LazySpec
 return {
 	"RedsXDD/neopywal.nvim",
 	lazy = false,
 	priority = 1000,
+	---@module "neopywal"
+	---@type NeopywalOptions
+	---@diagnostic disable-next-line: missing-fields
 	opts = {
 		transparent_background = true,
+		custom_highlights = {
+			LineNr = { fg = "dimmed" },
+		},
 		no_italic = true,
 		use_palette = {
 			dark = "wallust",
