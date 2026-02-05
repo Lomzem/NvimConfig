557077000f2d53c162970070bac4fed5f0fa1291
Lawjay Lee <lawjaylee1@icloud.com>
2026-02-05 13:32:47 -0800

    Just push 😝

--- after/ftplugin/tex.lua
+++ after/ftplugin/tex.lua
@@ -16,9 +16,9 @@ vim.api.nvim_create_autocmd("BufWritePost", {
 		local cmd = string.format("pdflatex %s; latexmk -c", file)
 		vim.fn.jobstart(cmd, {
 			on_exit = function()
-				vim.fs.rm(file_basename .. ".aux")
-				vim.fs.rm(file_basename .. ".log")
-				vim.fs.rm(file_basename .. ".out")
+				vim.fs.rm(file_basename .. ".aux", { force = true })
+				vim.fs.rm(file_basename .. ".log", { force = true })
+				vim.fs.rm(file_basename .. ".out", { force = true })
 			end,
 		})
 	end,
