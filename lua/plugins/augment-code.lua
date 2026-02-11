844ef2a008f88dfeef674e124023880010bc9d61
Lawjay Lee <lawjaylee1@icloud.com>
2026-02-11 12:19:10 -0800

    Just push 😝

--- lua/plugins/augment-code.lua
+++ lua/plugins/augment-code.lua
@@ -3,7 +3,9 @@ return {
 	enabled = true,
 	cmd = "Augment",
 	config = function()
-		vim.g.augment_workspace_folders = "~/coding/djsongmatch/"
+		vim.g.augment_workspace_folders = {
+			"/home/lomzem/dotfiles/.config/zsh",
+		}
 	end,
 	keys = {
 		{ "<leader>ac", "<cmd>Augment chat<cr>", mode = { "n", "v" } },
