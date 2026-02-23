cd54ab5bf648300d72457b827a6086e2f0007b30
Lawjay Lee <lawjaylee1@icloud.com>
2026-02-23 12:13:15 -0800

    fix mason

--- after/ftplugin/lex.lua
+++ after/ftplugin/lex.lua
@@ -1,28 +1,28 @@
 vim.api.nvim_create_autocmd("BufWritePost", {
 	pattern = "*.l",
 	callback = function()
-		local filename = vim.fn.expand("%:p")
-		local dirname = vim.fn.expand("%:h")
-		local lex_file = vim.fs.joinpath(dirname, "lex.yy.c")
-		vim.fn.jobstart({
-			"flex",
-			filename,
-		}, {
-			on_exit = function()
-				vim.fn.jobstart({
-					"gcc",
-					lex_file,
-					"-lfl",
-					"-o",
-					"foo",
-				}, {
-					on_stderr = function(_, data)
-						if data then
-							vim.notify(table.concat(data, "\n"))
-						end
-					end,
-				})
-			end,
-		})
+		-- local filename = vim.fn.expand("%:p")
+		-- local dirname = vim.fn.expand("%:h")
+		-- local lex_file = vim.fs.joinpath(dirname, "lex.yy.c")
+		-- vim.fn.jobstart({
+		-- 	"flex",
+		-- 	filename,
+		-- }, {
+		-- 	on_exit = function()
+		-- 		vim.fn.jobstart({
+		-- 			"gcc",
+		-- 			lex_file,
+		-- 			"-lfl",
+		-- 			"-o",
+		-- 			"foo",
+		-- 		}, {
+		-- 			on_stderr = function(_, data)
+		-- 				if data then
+		-- 					vim.notify(table.concat(data, "\n"))
+		-- 				end
+		-- 			end,
+		-- 		})
+		-- 	end,
+		-- })
 	end,
 })
