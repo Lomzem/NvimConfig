be8629262712df77e77d2a94366e1ddf4bc6b942
Lawjay Lee <lawjaylee1@icloud.com>
2026-02-23 12:13:15 -0800

    fix mason

--- lua/plugins/mason-tools-installer.lua
+++ lua/plugins/mason-tools-installer.lua
@@ -1,34 +1,33 @@
 ---@type LazySpec
 return {
 	"WhoIsSethDaniel/mason-tool-installer.nvim",
-	-- event = "VeryLazy",
-	config = function()
-		local mason_tools_installer = require("mason-tool-installer")
-		mason_tools_installer.setup({
-			auto_update = true,
-			ensure_installed = {
-				"air",
-				"clangd",
-				"cmake",
-				"gopls",
-				"jsonls",
-				"lua-language-server",
-				"mdformat",
-				"prettier",
-				"pyright",
-				"ruff",
-				"rustywind",
-				"stylua",
-				"tailwindcss-language-server",
-				"texlab",
-				"tinymist",
-				"tsgo",
-				"typstyle",
-				"yamlfmt",
-				"tex-fmt",
-			},
-		})
-		mason_tools_installer.clean()
-		mason_tools_installer.check_install(true, false)
-	end,
+	cmd = {
+		"MasonToolsClean",
+		"MasonToolsInstall",
+		"MasonToolsUpdate",
+	},
+	opts = {
+		auto_update = true,
+		ensure_installed = {
+			"air",
+			"clangd",
+			"cmake",
+			"gopls",
+			"jsonls",
+			"lua-language-server",
+			"mdformat",
+			"prettier",
+			"pyright",
+			"ruff",
+			"rustywind",
+			"stylua",
+			"tailwindcss-language-server",
+			"texlab",
+			"tinymist",
+			"tsgo",
+			"typstyle",
+			"yamlfmt",
+			"tex-fmt",
+		},
+	},
 }
