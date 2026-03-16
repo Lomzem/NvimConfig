0329dfec4143e41983a23828ce567fdd8cd2be87
Lawjay Lee <lawjaylee1@icloud.com>
2026-03-16 15:12:51 -0700

    render markdown insert

--- lua/plugins/render-markdown.lua
+++ lua/plugins/render-markdown.lua
@@ -1,50 +1,50 @@
 ---@type LazySpec
 return {
-	"MeanderingProgrammer/render-markdown.nvim",
-	dependencies = {
-		"latex-lsp/tree-sitter-latex",
-		"nvim-tree/nvim-web-devicons",
-	},
-	ft = {
-		"markdown",
-		"markdown_inline",
-	},
-	---@module 'render-markdown'
-	---@type render.md.UserConfig
-	opts = {
-		file_types = { "markdown", "markdown_inline", "tasks" },
-		anti_conceal = { enabled = false },
-		render_modes = { "n", "i", "v", "c" },
-		heading = {
-			icons = {},
-			sign = false,
-			position = "inline",
-			backgrounds = {
-				"",
-				"",
-				"",
-				"",
-				"",
-				"",
-			},
-		},
-		code = {
-			sign = false,
-			style = "normal",
-			language = true,
-			language_icon = true,
-			language_name = false,
-			-- highlight = "none",
-			-- highlight_inline = "none",
-			-- highlight_language = "none",
-			-- highlight_info = "none",
-			language_info = true,
-			-- highlight = "none",
-			-- highlight_inline = "none",
-			-- border = "none",
-		},
-		html = {
-			comment = { conceal = false },
-		},
-	},
+    "MeanderingProgrammer/render-markdown.nvim",
+    dependencies = {
+        "latex-lsp/tree-sitter-latex",
+        "nvim-tree/nvim-web-devicons",
+    },
+    ft = {
+        "markdown",
+        "markdown_inline",
+    },
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {
+        file_types = { "markdown", "markdown_inline", "tasks" },
+        anti_conceal = { enabled = false },
+        render_modes = { "n", "v", "c" },
+        heading = {
+            icons = {},
+            sign = false,
+            position = "inline",
+            backgrounds = {
+                "",
+                "",
+                "",
+                "",
+                "",
+                "",
+            },
+        },
+        code = {
+            sign = false,
+            style = "normal",
+            language = true,
+            language_icon = true,
+            language_name = false,
+            -- highlight = "none",
+            -- highlight_inline = "none",
+            -- highlight_language = "none",
+            -- highlight_info = "none",
+            language_info = true,
+            -- highlight = "none",
+            -- highlight_inline = "none",
+            -- border = "none",
+        },
+        html = {
+            comment = { conceal = false },
+        },
+    },
 }
