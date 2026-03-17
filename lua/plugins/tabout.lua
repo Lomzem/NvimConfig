f4fb286c6efc5aaf0f2597cec2fecd80f6b79da0
Lawjay Lee <lawjaylee1@icloud.com>
2026-03-17 16:42:15 -0700

    tabout

--- lua/plugins/tabout.lua
+++ lua/plugins/tabout.lua
@@ -1,9 +1,8 @@
 return {
 	"abecodes/tabout.nvim",
-	cond = false,
 	opts = {
-		tabkey = "<Tab>", -- key to trigger tabout, set to an empty string to disable
-		backwards_tabkey = "<S-Tab>", -- key to trigger backwards tabout, set to an empty string to disable
+		tabkey = "<Down>", -- key to trigger tabout, set to an empty string to disable
+		backwards_tabkey = "<Up>", -- key to trigger backwards tabout, set to an empty string to disable
 		act_as_tab = true, -- shift content if tab out is not possible
 		act_as_shift_tab = false, -- reverse shift content if tab out is not possible (if your keyboard/terminal supports <S-Tab>)
 		default_tab = "<C-t>", -- shift default action (only at the beginning of a line, otherwise <TAB> is used)
