936f5bf0ac0249cf67321a6dda2e74777273a632
Lawjay Lee <lawjaylee1@icloud.com>
2026-03-25 23:31:40 -0700

    Just push 😝

--- lua/plugins/marks-nvim.lua
+++ lua/plugins/marks-nvim.lua
@@ -1,15 +1,19 @@
 return {
 	"chentoast/marks.nvim",
+	event = "VeryLazy",
 	keys = {
 		"m",
 		"dm",
-		"<a-m>",
+		{
+			"<a-m>",
+			"<Plug>(Marks-next)zz",
+		},
 	},
 	opts = {
 		mappings = {
 			set_next = "m,",
 			delete_buf = "dm<leader>",
-			next = "<a-m>",
+			-- next = "<a-m>",
 		},
 	},
 }
