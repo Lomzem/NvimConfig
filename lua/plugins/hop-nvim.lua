e08d9d62d0a1a813618e3f3c59a76797ea60d196
Lawjay Lee <lawjaylee1@icloud.com>
2026-04-23 01:23:33 -0700

    more hop

--- lua/plugins/hop-nvim.lua
+++ lua/plugins/hop-nvim.lua
@@ -14,9 +14,7 @@ return {
 	keys = {
 		{
 			"<cr>",
-			function()
-				require("hop").hint_lines_skip_whitespace()
-			end,
+			"<cmd>HopLineStart<cr>",
 			mode = { "n", "x", "o" },
 		},
 		{
@@ -26,18 +24,22 @@ return {
 				local hop = require("hop")
 
 				local opts = setmetatable({}, { __index = hop.opts })
-				hop.hint_with_regex(
-					require("hop.jump_regex").regex_by_line_start_skip_whitespace(),
-					opts,
-					function(jump_target)
-						local target_line = jump_target.cursor.row
-						local from_line = math.min(start_line, target_line)
-						local to_line = math.max(start_line, target_line)
-						vim.cmd(string.format("%d,%dd", from_line, to_line))
-					end
-				)
+				hop.hint_with_regex(require("hop.jump_regex").by_line_start(), opts, function(jump_target)
+					local target_line = jump_target.cursor.row
+					local from_line = math.min(start_line, target_line)
+					local to_line = math.max(start_line, target_line)
+					vim.cmd(string.format("%d,%dd", from_line, to_line))
+				end)
 			end,
 			mode = { "n" },
+		},
+		{
+			"<cr>",
+			function()
+				vim.cmd("HopLine")
+				vim.cmd("norm V")
+			end,
+			mode = { "v" },
 		},
 	},
 }
